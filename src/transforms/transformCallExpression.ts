4ea3402da5948af09232a31b65b9d8097da5dbfc
Laud Boateng <capedbojji@gmail.com>
2024-04-27 15:24:41 +0000

    Apr 27, 2024, 11:24 AM

--- src/transforms/transformCallExpression.ts
+++ src/transforms/transformCallExpression.ts
@@ -2,5 +2,9 @@ import * as ts from 'typescript';
 
 
 function visitCallExpression(node: ts.CallExpression) {
-    
+    // 
+    const expression = node.expression;
+    if (ts.isPropertyAccessExpression(expression) && expression.expression.getText() === "Yeagar" && expression.name.getText() === "addPath") {
+        
+    }
 }
