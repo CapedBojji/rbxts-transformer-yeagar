f275daa5800265fbcc4ae759f9ebe0820084f2c8
Laud Boateng <capedbojji@gmail.com>
2024-04-29 04:26:14 +0000

    Apr 29, 2024, 12:26 AM

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,22 @@
 import ts from "typescript";
 import { TransformContext, TransformerConfig } from "./transformer";
+import exp from "constants";
 
 /**
  * The transformer entry point.
  * This provides access to necessary resources and the user specified configuration.
  */
-export default function (program: ts.Program, config: TransformerConfig) {
-	return (transformationContext: ts.TransformationContext): ((file: ts.SourceFile) => ts.Node) => {
-		const context = new TransformContext(program, transformationContext, config);
-		return (file) => context.transform(file);
-	};
+export default function(program: ts.Program, config: TransformerConfig) {
+  return (
+    transformationContext: ts.TransformationContext
+  ): ((file: ts.SourceFile) => ts.Node) => {
+    const context = new TransformContext(
+      program,
+      transformationContext,
+      config
+    );
+    return (file) => context.transform(file);
+  };
 }
+
+export function $path(str: string) {}
